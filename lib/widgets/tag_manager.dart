2c1d46da1ee9424456620fe67d71ceae654022d9
JackLindgren <jtlindgren88@gmail.com>
2021-05-15 16:59:19 -0700

    refactor bike-view tags

--- lib/widgets/tag_manager.dart
+++ lib/widgets/tag_manager.dart
@@ -6,7 +6,7 @@ import '../helpers/tags.dart';
 
 final GlobalKey<TagsState> _tagStateKey = GlobalKey<TagsState>();
 
-Widget showTags(BuildContext context, BikeDTO thisBike) {
+Widget editTags(BuildContext context, BikeDTO thisBike) {
   double _fontSize = 14;
   return Tags(
     key: _tagStateKey,
@@ -30,6 +30,41 @@ Widget showTags(BuildContext context, BikeDTO thisBike) {
           print(tag);
           thisBike.tags[tag.title] = tag.active;
         },
+        onLongPressed: (tag) => print(tag),
+      );
+    },
+  );
+}
+
+Widget loadTags(BuildContext context, Map currentTags) {
+  // convert Map to list of strings for tags that are set
+  List<String> tags = [];
+  currentTags.forEach((key, value) {
+    if (value) {
+      tags.add(key);
+    }
+  });
+  double _fontSize = 14;
+  return Tags(
+    key: _tagStateKey,
+    itemCount: tags.length, // required
+    itemBuilder: (int index) {
+      final tag = tags[index];
+
+      return ItemTags(
+        // Each ItemTags must contain a Key. Keys allow Flutter to
+        // uniquely identify widgets.
+        key: Key(index.toString()),
+        active: true,
+        pressEnabled: false,
+        //color: Colors.blue,
+        activeColor: Colors.blueAccent,
+        index: index, // required
+        title: tag,
+        textStyle: TextStyle(
+          fontSize: _fontSize,
+        ),
+        combine: ItemTagsCombine.withTextBefore,
         onLongPressed: (tag) => print(tag),
       );
     },
